3f4031d44349440598a6d4d1989e67b5ef81da58
whitehat4u <0101whitehat0101@gmail.com>
2018-09-24 16:05:15 -0500

    comit
    comit

---
+++ iteraciones/iteraciones.js
@@ -0,0 +1,38 @@
+/* SENTENCIAS Y DECLARACIONES POR CATEGORÍA
+
+ ITERACIONES: BUCLES.
+
+WHILE - DO WHILE - FOR
+
+WHILE:
+solo ejecuta el bloque si cumple la condición.
+
+DO WHILE:
+Al menos una vez, ejecuta el bloque.
+
+FOR:
+exp Inicial(expresiones de Asignacion, declarion de variables); condición(true o false); exp Final(expresion a evaluar, incremento);{
+    bloque;
+}
+ 
+*/
+
+
+// while (a<10) {
+//     alert("Hola whitehat4u "  + a);
+//     a++;
+//     console.log(a);
+// }
+
+// do {
+//     console.log("Hola Whitehat4u");
+//     a++;
+//     console.log(a);
+// } while (a<10);
+
+// console.log(a);
+
+for (let a = 0; a < 10; a++) {
+    console.log("whitehat4u " + a);
+}
+
